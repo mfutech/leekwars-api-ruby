b39c3ee245ccc41cfc95873340edd57cdd1e047f
mfutech <mfutech@users.noreply.github.com>
2019-08-25 14:36:23 +0200

    handle of runtimerror in automate.rb

--- automate.rb
+++ automate.rb
@@ -9,46 +9,54 @@ CONFIG_FILE = File.join File.dirname(__FILE__), 'config.yml'
 
 def do_all(tokens)
 	tokens.each do |farmer, token| 
-		puts "--- Farmer: #{farmer} ---------"
-		api = LeekAPI.new token
-		puts "--- Farmer fight"
-		api.do_farmer_fights
-		puts "--- Team fight"
-		api.do_team_fights
-		puts "--- Solo fight"
-		##garden = api.get('garden/get')['garden']
-		api.leeks.each do |leek|
-			while api.do_solo_fight(leek) do
+		begin
+			puts "--- Farmer: #{farmer} ---------"
+			api = LeekAPI.new token
+			puts "--- Farmer fight"
+			api.do_farmer_fights
+			puts "--- Team fight"
+			api.do_team_fights
+			puts "--- Solo fight"
+			##garden = api.get('garden/get')['garden']
+			api.leeks.each do |leek|
+				while api.do_solo_fight(leek) do
+				end
 			end
+		rescue RuntimeError => e
+			puts "Problem with token #{e}"
 		end
 	end
 	true
 end
 
 def do_register(tokens)
-	tokens.each do |farmer, token| 
-		puts "--- Farmer: #{farmer} ---------"
-		api = LeekAPI.new token
-		puts "--- register-tournament"
-		begin
-			api.get "farmer/register-tournament"
+	tokens.each do |farmer, token|
+		begin 
+			puts "--- Farmer: #{farmer} ---------"
+			api = LeekAPI.new token
+			puts "--- register-tournament"
+			begin
+				api.get "farmer/register-tournament"
+			rescue RuntimeError => e
+				puts e
+			end
+			api.leeks.each do |leek|
+				begin
+					api.get "leek/register-tournament/#{leek}"
+				rescue RuntimeError => e
+					puts e
+				end
+			end
+			api.get_garden['my_compositions'].map{|x|x['id']}.each do |compo_id|
+				puts "--- register-team #{compo_id}"
+				begin
+					api.get "team/register-tournament/#{compo_id}"
+				rescue RuntimeError => e
+					puts e
+				end
+			end
 		rescue RuntimeError => e
-			puts e
-		end
-		api.leeks.each do |leek|
-			begin
-				api.get "leek/register-tournament/#{leek}"
-			rescue RuntimeError => e
-				puts e
-			end
-		end
-		api.get_garden['my_compositions'].map{|x|x['id']}.each do |compo_id|
-			puts "--- register-team #{compo_id}"
-			begin
-				api.get "team/register-tournament/#{compo_id}"
-			rescue RuntimeError => e
-				puts e
-			end
+			puts "Problem with token #{e}"
 		end
 	end
 end
@@ -63,4 +71,3 @@ if __FILE__ == $0 then
 	do_all tokens
 	do_register tokens
 end
-
